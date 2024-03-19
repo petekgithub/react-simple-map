3c1528be04cc652afbead9d5921ba23c24d88263
Petek Savas <petekshamitbeyli@gmail.com>
2024-03-19 16:55:39 +0300

    App-test-works-well

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,19 @@
-import "@testing-library/jest-dom/extend-expect";
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom"; // Değişiklik burada
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../store/store"; // Redux store'u import edin
 import App from "../App";
 
 test("App component renders correctly", () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 
   // MapChart bileşeninin varlığını kontrol et
   const mapChart = screen.getByTestId("map-chart");
   expect(mapChart).toBeInTheDocument();
-
-  // MapChart bileşenine tıklama yap
-  userEvent.click(mapChart);
-
-  // ReactTooltip bileşeninin varlığını kontrol et
-  const reactTooltip = screen.getByTestId("react-tooltip");
-  expect(reactTooltip).toBeInTheDocument();
 });
